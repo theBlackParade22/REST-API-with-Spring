cd7a434254c2e877759d2c182620c0d08aed434e
Hari Saresh <hari.saresh@gmail.com>
2020-08-07 15:41:46 +0800

    First commit

---
+++ src/main/java/com/codewithhari/RESTdemo/ResTdemoApplication.java
@@ -0,0 +1,13 @@
+package com.codewithhari.RESTdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class ResTdemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ResTdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codewithhari/RESTdemo/ResTdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithhari.RESTdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
